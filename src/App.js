3412dbf9a7a9800780f36acfb97e17e16f017241
nya-n <miyazakibucho@icloud.com>
2022-12-02 06:40:49 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <p>修正しました</p>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
